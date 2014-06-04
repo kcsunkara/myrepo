47e92ada588f08cf529aba316a1a61d8732ae494
KCSunkara <KCSunkara@KCSunkara-PC>
2014-06-04 22:29:15 +0530

    policy info

--- ReplicaUIDashboard/src/main/java/com/cognizant/ui/dao/DashboardServiceImpl.java
+++ ReplicaUIDashboard/src/main/java/com/cognizant/ui/dao/DashboardServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cognizant.ui.beans.CustomPolicySiteInfo;
 import com.cognizant.ui.beans.SearchCriteria;
 import com.cognizant.ui.json.Customer;
 import com.cognizant.ui.model.AssetDetails;
@@ -46,62 +47,69 @@ public class DashboardServiceImpl implements DashboardService {
 	}
 
 	/**
-	 * This method will fetch the counts of the jobs from batchjobs table as per the status.
+	 * This method will fetch the counts of the jobs from batchjobs table as per
+	 * the status.
+	 * 
 	 * @param modelMap
 	 * @param jobDate
 	 * @return
 	 */
 	public CustomJobBean getJobCountForAllJobTypes(String jobDate) {
-		CustomJobBean cb=dashBoardDao.getJobCountForAllJobTypes(jobDate);
+		CustomJobBean cb = dashBoardDao.getJobCountForAllJobTypes(jobDate);
 		return cb;
 	}
-	
-	 /**
-     * This method will fetch the details of jobs based on the jobType selected.
-     * @param jobType
-     * @param day
-     * @param jobStatus
-     * @return
-     */
-	public List getJobsByType(String jobType,int day,String jobStatus) {
-		List cb=dashBoardDao.getJobsByType(jobType,day,jobStatus);
-		return cb;
-	}
+
 	/**
-     * This method will fetch the customer and customer instances details for the
-     * selected customer id.
-     * 
-     * @param criteria
-     * @retun list of asset details.
-     */
-    public List<Customer> getCustomerNames() {
-        List<Customer> customerDetails = dashBoardDao.getCustomerNames();
-        return customerDetails;
-    }
-    
-    
-    public CustomJobBean getJobDetails(Long jobId) {
-        CustomJobBean customerDetails = dashBoardDao.getJobDetails(jobId);
-        return customerDetails;
-    }
-    
-    public List<AssetDetails> getInvalidAssets(){
-        List<AssetDetails> invaliedAssets=dashBoardDao.getInvalidAssets();
-        return invaliedAssets;
-    }
-    
- public Map<String,Integer> getLocationsIds() {
-        
-        Map<String,Integer> locationIds=dashBoardDao.getLocationsIds();
-        return locationIds;
-    }
- 
- /**
-  * This method will fetch all the policies in the DB.
-  * @retun list policies.
-  */
- public List<Policy> getPolicies() {
-	 return dashBoardDao.getPolicies();
- }
+	 * This method will fetch the details of jobs based on the jobType selected.
+	 * 
+	 * @param jobType
+	 * @param day
+	 * @param jobStatus
+	 * @return
+	 */
+	public List getJobsByType(String jobType, int day, String jobStatus) {
+		List cb = dashBoardDao.getJobsByType(jobType, day, jobStatus);
+		return cb;
+	}
+
+	/**
+	 * This method will fetch the customer and customer instances details for
+	 * the selected customer id.
+	 * 
+	 * @param criteria
+	 * @retun list of asset details.
+	 */
+	public List<Customer> getCustomerNames() {
+		List<Customer> customerDetails = dashBoardDao.getCustomerNames();
+		return customerDetails;
+	}
+
+	public CustomJobBean getJobDetails(Long jobId) {
+		CustomJobBean customerDetails = dashBoardDao.getJobDetails(jobId);
+		return customerDetails;
+	}
+
+	public List<AssetDetails> getInvalidAssets() {
+		List<AssetDetails> invaliedAssets = dashBoardDao.getInvalidAssets();
+		return invaliedAssets;
+	}
+
+	public Map<String, Integer> getLocationsIds() {
+		Map<String, Integer> locationIds = dashBoardDao.getLocationsIds();
+		return locationIds;
+	}
+
+	/**
+	 * This method will fetch all the policies in the DB.
+	 * 
+	 * @retun list policies.
+	 */
+	public List<Policy> getPolicies() {
+		return dashBoardDao.getPolicies();
+	}
+
+	public List<CustomPolicySiteInfo> getSiteInfo(Long pid) {
+		return dashBoardDao.getSiteInfo(pid);
+	}
 
 }
