8d6fd35e0b935fd756a5e43c56af2ce0e9accf88
kcsunkara <kcsunkara@>
2014-09-08 15:17:24 +0530

    quartz tables

--- dl-df-bridge/src/main/java/com/hbo/itdmg/jobs/DigitalFusionLookupJob.java
+++ dl-df-bridge/src/main/java/com/hbo/itdmg/jobs/DigitalFusionLookupJob.java
@@ -13,12 +13,22 @@ import org.springframework.transaction.annotation.Transactional;
 public class DigitalFusionLookupJob  extends QuartzJobBean {
 
 	private static Logger LOG = Logger.getLogger(DigitalFusionLookupJob.class.getName());
+	
+	int daysToArchiveSuspended;
 
 	@Override
 	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	protected void executeInternal(JobExecutionContext arg0)
 			throws JobExecutionException {
 		LOG.debug("DigitalFusionLookupJob process initiated on: "+Calendar.getInstance().getTime());
+	}
+
+	public int getDaysToArchiveSuspended() {
+		return daysToArchiveSuspended;
+	}
+
+	public void setDaysToArchiveSuspended(int daysToArchiveSuspended) {
+		this.daysToArchiveSuspended = daysToArchiveSuspended;
 	}
 
 }
