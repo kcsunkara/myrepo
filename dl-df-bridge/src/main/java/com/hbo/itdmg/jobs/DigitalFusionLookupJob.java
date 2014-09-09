33682094eacc1b5c84b40f152978b117bac0fbb7
KCSunkara <KCSunkara@>
2014-09-09 22:49:55 +0530

    oracle and mysql support

--- dl-df-bridge/src/main/java/com/hbo/itdmg/jobs/DigitalFusionLookupJob.java
+++ dl-df-bridge/src/main/java/com/hbo/itdmg/jobs/DigitalFusionLookupJob.java
@@ -5,30 +5,22 @@ import java.util.Calendar;
 import org.apache.log4j.Logger;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.StatefulJob;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 
-public class DigitalFusionLookupJob  extends QuartzJobBean {
+public class DigitalFusionLookupJob  extends QuartzJobBean { // implements StatefulJob {
 
 	private static Logger LOG = Logger.getLogger(DigitalFusionLookupJob.class.getName());
 	
-	int daysToArchiveSuspended;
-
 	@Override
-	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
+	@Transactional //(propagation = Propagation.REQUIRED, readOnly = false)
 	protected void executeInternal(JobExecutionContext arg0)
 			throws JobExecutionException {
 		LOG.debug("DigitalFusionLookupJob process initiated on: "+Calendar.getInstance().getTime());
-	}
-
-	public int getDaysToArchiveSuspended() {
-		return daysToArchiveSuspended;
-	}
-
-	public void setDaysToArchiveSuspended(int daysToArchiveSuspended) {
-		this.daysToArchiveSuspended = daysToArchiveSuspended;
+		System.out.println("DigitalFusionLookupJob process initiated on: "+Calendar.getInstance().getTime());
 	}
 
 }
