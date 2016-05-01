1fa9e222218ae1c7c3b28f6cbee8cd79f518766b
novisys <KCSunkara@KCSunkara-PC>
2016-05-01 22:28:26 +0530

    jpa repo add

---
+++ esspring/src/main/java/com/csc/repository/EmpRepositoryJPA.java
@@ -0,0 +1,11 @@
+package com.csc.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.csc.doc.Emp;
+
+@Repository
+public interface EmpRepositoryJPA extends CrudRepository<Emp, Integer> {
+	
+}
